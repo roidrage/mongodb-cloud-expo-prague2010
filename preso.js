890db9bb8edb2a1771c63ad7fb61cc1f6b15a90f
Mathias Meyer <meyer@paperplanes.de>
2010-06-21 14:54:33 +0200

    Increased timeout.

--- preso.js
+++ preso.js
@@ -1,4 +1,4 @@
 setTimeout(function() {
   $('pre.sh_sourceCode').wrap('<div style="width: 100%; text-align: center;"/>');
   $('pre.sh_sourceCode').css('display', 'inline-block').css('margin', '20px auto').css('text-align', 'left');
-}, 1000)
+}, 5000)
